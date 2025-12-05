a928a1a42f14801dee450af321deaf7706fa4d1e
Emma10003 <5emma10003@gmail.com>
2025-12-05 09:39:26 +0900

    프로젝트 시작

---
+++ front-end/src/App.js
@@ -0,0 +1,26 @@
+// ============================================
+// src/App.js
+// TODO: React Router 설정하기
+// - BrowserRouter, Routes, Route import 하기
+// - LoginPage, FeedPage, UploadPage import 하기
+// - PrivateRoute import 하기
+// - /login 경로에 LoginPage 연결
+// - /feed 경로에 FeedPage 연결 (PrivateRoute로 보호)
+// - /upload 경로에 UploadPage 연결 (PrivateRoute로 보호)
+// - 기본 경로(/)는 /login으로 리다이렉트
+// ============================================
+
+import React from 'react';
+import './App.css';
+
+// TODO: 필요한 컴포넌트들을 import 하세요
+
+function App() {
+    return (
+        <div>
+            {/* TODO: Router 설정을 완성하세요 */}
+        </div>
+    );
+}
+
+export default App;
